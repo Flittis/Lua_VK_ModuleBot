d49bb3ad9a3f2880d25041e124a0b2f50f1cce2a
Nikolay Flitov <mega.aang@gmail.com>
2020-04-14 02:45:30 +0300

    Moved timeout in stickers trigger to config

--- config.lua
+++ config.lua
@@ -4,6 +4,7 @@ return {
     deleteTriggerAll = "/delall",
     deleteEditTo = "ᅠ",
     stickerTriggerWord = "[фf][лl][иi][тt]",
-    stickerTriggerChats = {82, 289, 290, 254, 292},
-    stickerTriggerStickers = {11195, 6330, 4380, 4381, 4387, 4382, 4384, 4386, 10371, 10354, 8118, 18351, 16332, 14153, 12706, 17508, 18950, 17673, 18973, 18952, 13250, 18945, 18948}
+    stickerTriggerChats = {1, 2, 3},
+    stickerTriggerStickers = {11195, 6330, 4380, 4381, 4387, 4382, 4384, 4386, 10371},
+    stickerTriggerTime = 5
 }
--- modules/trigger.lua
+++ modules/trigger.lua
@@ -1,5 +1,5 @@
 local config = ...
-local stickerTriggerWord, stickerTriggerChats, stickerTriggerStickers = config.stickerTriggerWord or '', config.stickerTriggerChats or {}, config.stickerTriggerStickers or {1}
+local stickerTriggerWord, stickerTriggerChats, stickerTriggerStickers, stickersTimeoutTime = config.stickerTriggerWord or '', config.stickerTriggerChats or {}, config.stickerTriggerStickers or {1}, stickerTriggerTime or 5
 local timeout = {}
 
 local obj = {}
@@ -13,7 +13,7 @@ function obj.func(msg)    -- creating callback function
     if(timeout[msg.peer_id] == nil and msg.out == false and msg.body:lower():find(stickerTriggerWord) and (not stickerTriggerChats[1] or isInArray(stickerTriggerChats, msg.chat_id))) then
         vk.call('messages.send', { peer_id = msg.peer_id, sticker_id = stickerTriggerStickers[math.random(1, #stickerTriggerStickers)] })
 
-        timeout[msg.peer_id] = os.time() + 300
+        timeout[msg.peer_id] = os.time() + (timeoutTime * 60)
     end
 end
 
