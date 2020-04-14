b7a29858dba5209151fb5ffef3d010d0c3027964
Nikolay Flitov <mega.aang@gmail.com>
2020-04-14 03:01:18 +0300

    Some trigger words in sticker triggers

--- config.lua
+++ config.lua
@@ -3,7 +3,7 @@ return {
     deleteTrigger = "ой",
     deleteTriggerAll = "/delall",
     deleteEditTo = "ᅠ",
-    stickerTriggerWord = "[фf][лl][иi][тt]",
+    stickerTriggerWords = {"флит", "flittis"},
     stickerTriggerChats = {1, 2, 3},
     stickerTriggerStickers = {11195, 6330, 4380, 4381, 4387, 4382, 4384, 4386, 10371},
     stickerTriggerTime = 5
--- modules/trigger.lua
+++ modules/trigger.lua
@@ -1,5 +1,5 @@
 local config = ...
-local stickerTriggerWord, stickerTriggerChats, stickerTriggerStickers, stickersTimeoutTime = config.stickerTriggerWord or '', config.stickerTriggerChats or {}, config.stickerTriggerStickers or {1}, stickerTriggerTime or 5
+local stickerTriggerWords, stickerTriggerChats, stickerTriggerStickers, stickersTimeoutTime = config.stickerTriggerWords or {'ping'}, config.stickerTriggerChats or {}, config.stickerTriggerStickers or {1}, stickerTriggerTime or 5
 local timeout = {}
 
 local obj = {}
@@ -10,7 +10,7 @@ function isInArray(a, v) for i = 1, #a do if a[i] == v then return true end end 
 function obj.func(msg)    -- creating callback function
     if(timeout[msg.peer_id] and os.time() > timeout[msg.peer_id]) then timeout[msg.peer_id] = nil end
 
-    if(timeout[msg.peer_id] == nil and msg.out == false and msg.body:lower():find(stickerTriggerWord) and (not stickerTriggerChats[1] or isInArray(stickerTriggerChats, msg.chat_id))) then
+    if(timeout[msg.peer_id] == nil and msg.out == false and isInArray(stickerTriggerWords, msg.body:lower()) and (not stickerTriggerChats[1] or isInArray(stickerTriggerChats, msg.chat_id))) then
         vk.call('messages.send', { peer_id = msg.peer_id, sticker_id = stickerTriggerStickers[math.random(1, #stickerTriggerStickers)] })
 
         timeout[msg.peer_id] = os.time() + (timeoutTime * 60)
